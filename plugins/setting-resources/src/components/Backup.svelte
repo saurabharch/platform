fd6cf5715c73ac801350cf8ec21de6e9ea99ec7c
Alexey Zinoviev <alexey.zinoviev@xored.com>
2025-01-15 17:27:44 +0400

    uberf-8425: fixes Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>

--- plugins/setting-resources/src/components/Backup.svelte
+++ plugins/setting-resources/src/components/Backup.svelte
@@ -28,7 +28,7 @@
   const backupUrl = getMetadata(setting.metadata.BackupUrl) ?? ''
   const token = getMetadata(presentation.metadata.Token) ?? ''
   const owner = hasAccountRole(getCurrentAccount(), AccountRole.Owner)
-  const workspaceId = getMetadata(presentation.metadata.WorkspaceId) ?? ''
+  const workspaceUuid = getMetadata(presentation.metadata.WorkspaceUuid) ?? ''
 
   let backupInfo:
   | {
@@ -81,7 +81,7 @@
   async function updateBackupInfo (): Promise<void> {
     if (owner && backupUrl !== '') {
       try {
-        const response = await fetch(`${backupUrl}/${workspaceId}/index.json`, {
+        const response = await fetch(`${backupUrl}/${workspaceUuid}/index.json`, {
           method: 'GET',
           headers: {
             Authorization: `Bearer ${token}`
@@ -116,7 +116,7 @@
     return ''
   }
   function getFileUrl (filename: string): string {
-    return `${backupUrl}/${workspaceId}/${filename}`
+    return `${backupUrl}/${workspaceUuid}/${filename}`
   }
 
   async function downloadFile (filename: string): Promise<void> {
